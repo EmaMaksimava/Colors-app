52582e1ba2dda1e287a3564182d79bb9454ed837
Emma Maksimava <cielvibritania@gmail.com>
2022-10-27 10:34:31 +0200

    feat: create luminance value

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ function setRandomColors() {
   listOfCols.forEach(col => {
     const nameColor = col.querySelector('.name-color');
     const randomColor = chroma.random();
+    const lockColor = col.querySelector('i')
     col.style.background = randomColor;
     nameColor.innerText = randomColor;
+    setTextColor(nameColor, randomColor);
+    setTextColor(lockColor, randomColor);
   })
 }
 
 setRandomColors();
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
 
 function generateColor() {
   let colorValue = '';
