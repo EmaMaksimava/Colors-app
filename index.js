53717dfabf5ad2246bc10f4c1cff3e435bdfb50e
Emma Maksimava <cielvibritania@gmail.com>
2022-10-27 11:21:05 +0200

    feat: add change lock-icons

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const listOfCols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  if (event.code === 'Space') {
+    setRandomColors();
+  }
+})
+
+document.addEventListener('click', (event) => {
+  console.log(event.target.dataset);
+  const type = event.target.dataset.type;
+
+  if(type === 'lock') {
+    const node = event.target.tagName.toLowerCase() === 'i'
+    ? event.target
+    : event.target.children[0];
+
+    node.classList.toggle('fa-lock');
+    node.classList.toggle('fa-lock-open');
+  }
+})
+
 function setRandomColors() {
   listOfCols.forEach(col => {
     const nameColor = col.querySelector('.name-color');
